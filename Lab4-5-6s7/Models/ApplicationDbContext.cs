0c2c7fbc3b774c07e81b84cdd8893cfa1447875b
thang nguyen <nguvatva456@gmail.com>
2021-07-08 10:19:24 +0700

    BT-THLTWeb

--- Lab4-5-6s7/Models/ApplicationDbContext.cs
+++ Lab4-5-6s7/Models/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Lab4_5_6s7.Models
     {
             public DbSet<Course> Courses { get; set; }
             public DbSet<Category> Categories { get; set; }
+            public DbSet<Attendance> Attendances { get; set; }
             public ApplicationDbContext()
                 : base("DefaultConnection", throwIfV1Schema: false)
             {
@@ -20,6 +21,14 @@ namespace Lab4_5_6s7.Models
             {
                 return new ApplicationDbContext();
             }
-        
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Attendance>()
+                    .HasRequired(a => a.Course)
+                    .WithMany()
+                    .WillCascadeOnDelete(false);
+                base.OnModelCreating(modelBuilder);
+            }
+
     }
 }
